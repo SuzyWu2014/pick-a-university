eb67901d6a57563c68028970eb3a19ee3fefe3d8
SuzyWu2014 <shujinwu@onid.oregonstate.edu>
2016-06-28 18:04:27 -0700

    count addmission for 211/985 universities

--- analyzer/university_score_analyze.py
+++ analyzer/university_score_analyze.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 import pandas
 from pprint import pprint
+import numpy
 
 
 def create_df_university_scores():
@@ -28,7 +29,39 @@ def merge_ranking(df_university):
     return df_university
 
 
+def merge985_211(df_university):
+    list_985 = pandas.read_csv('data/university_985_list.csv', encoding="utf-8")
+    list_211 = pandas.read_csv('data/university_211_list.csv', encoding='utf-8')
+    list_985 = list_985['985']
+    list_211 = list_211['211']
+    df_university['is_985'] = numpy.where(df_university['university'].isin(list_985), '985', 'not_985')
+    df_university['is_211'] = numpy.where(df_university['university'].isin(list_211), '211', 'not_211')
+    return df_university
+
+
+def merge_admission(df_university):
+    df_admission = pandas.read_json('data/admission_2012_2013.json', orient='records')
+    df_university = df_university.merge(
+        df_admission,
+        how='left',
+        left_on=['year', 'university'],
+        right_on=['year', 'university']).drop(['stu_type', 'avg_score_y'], axis=1).fillna(0)
+    return df_university
+
+
+def count_admission(df_university):
+    grouped = df_university.groupby(['is_985', 'is_211', 'year'], axis=0)
+    # print grouped.count()
+    admission_count = grouped['admission_count'].agg({'admission_count': numpy.sum, 'university': numpy.count_nonzero})
+    # print admission_count
+    admission_count.to_csv('report/admission_count_2012_2013.csv', encoding="utf-8", index=False)
+    # print grouped['is_985'].agg([numpy.count])
+    # print grouped['admission_count'].agg([numpy.sum])
+
 if __name__ == '__main__':
     df_university = create_df_university_scores()
     df_university = merge_ranking(df_university)
-    export_to_csv(df_university, 'report/all_university_scores_with_ranking.csv')
+    df_university = merge985_211(df_university)
+    df_university = merge_admission(df_university)
+    count_admission(df_university)
+    # export_to_csv(df_university, 'report/all_university_scores_with_ranking.csv')
