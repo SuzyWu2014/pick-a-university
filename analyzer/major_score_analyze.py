12d331dabb88904f35d1785ffc1bc81633f51dd1
SuzyWu2014 <shujinwu@onid.oregonstate.edu>
2016-06-28 18:26:12 -0700

    fix: line too long

--- analyzer/major_score_analyze.py
+++ analyzer/major_score_analyze.py
@@ -1,27 +1,40 @@
 # -*- coding: utf-8 -*-
 import pandas
 import re
-from pprint import pprint
+# from pprint import pprint
 import numpy
 
 
 def group_major():
-    df_major = pandas.read_json("data/major_scores_2011_2015.json", orient='records')
-    df_major = df_major.sort_values(['university', 'major', 'year'], ascending=[0, 0, 1]).drop_duplicates()[['university', 'year', 'major', 'avg_score']]
+    df_major = pandas.read_json(
+        "data/major_scores_2011_2015.json", orient='records')
+    df_major = df_major.sort_values(
+        ['university', 'major', 'year'],
+        ascending=[0, 0, 1]).drop_duplicates()[[
+            'university', 'year', 'major', 'avg_score']]
     df_major.rename(columns={'avg_score': 'avg_major_score'}, inplace=True)
-    df_major['year'] = df_major['year'].str.replace(pat=u'年', repl='').astype(numpy.int32)
+    df_major['year'] = df_major['year'].str.replace(
+        pat=u'年', repl='').astype(numpy.int32)
     return df_major
 
 
 def merge_ranking(df_major):
-    df_ranking = pandas.read_csv('data/scores_ranking_2013_2015.csv', encoding='utf-8')
-    df_major = df_major.merge(df_ranking, how='left', left_on=['year', 'avg_major_score'], right_on=['year', 'grade']).drop('grade', axis=1).fillna(0)
+    df_ranking = pandas.read_csv(
+        'data/scores_ranking_2013_2015.csv', encoding='utf-8')
+    df_major = df_major.merge(
+        df_ranking, how='left',
+        left_on=['year', 'avg_major_score'],
+        right_on=['year', 'grade']).drop(
+            'grade', axis=1).fillna(0)
     return df_major
 
 
 def merge_university(df_major):
-    df_university = pandas.read_csv('report/university_scores.csv', encoding='utf-8')
-    df_major = df_major.merge(df_university, how='left', left_on=['year', 'university'], right_on=['year', 'university'])
+    df_university = pandas.read_csv(
+        'report/university_scores.csv', encoding='utf-8')
+    df_major = df_major.merge(
+        df_university, how='left',
+        left_on=['year', 'university'], right_on=['year', 'university'])
     return df_major
 
 
@@ -33,7 +46,8 @@ def filter_cs(df_major):
 
 
 def filter_ranking(df_major, low, high):
-    df_major = df_major[(df_major['stu_ranking'] > low) & (df_major['stu_ranking'] < high)]
+    df_major = df_major[
+        (df_major['stu_ranking'] > low) & (df_major['stu_ranking'] < high)]
     return df_major
 
 
@@ -45,8 +59,10 @@ if __name__ == '__main__':
     df_major = group_major()
     df_major = merge_ranking(df_major)
     df_major = merge_university(df_major)
-    df_major = df_major.sort_values(['university', 'year', 'major'], ascending=[0, 1, 0])
-    # df_major = filter_cs(df_major).sort_values(['university', 'year'], ascending=[0, 1])
+    df_major = df_major.sort_values(
+        ['university', 'year', 'major'], ascending=[0, 1, 0])
+    # df_major = filter_cs(df_major).sort_values(
+    #   ['university', 'year'], ascending=[0, 1])
     # print df_major.head()
     # df_major = filter_ranking(df_major, 5000, 15000)
     export_to_csv('report/all_major.csv', df_major)
--- analyzer/university_score_analyze.py
+++ analyzer/university_score_analyze.py
@@ -6,15 +6,21 @@ import numpy
 
 def create_df_university_scores():
     '''
-    convert json data into pandas dataframe, and sort the data by unversity-year
+    convert json data into pandas dataframe,
+    and sort the data by unversity-year
     '''
-    df_university = pandas.read_json("data/university_scores_2011_2015.json", orient='records')
-    df_university = df_university.sort_values(['university', 'year'], ascending=[0, 1]).drop_duplicates()[['university', 'year', 'score_line', 'avg_score', 'score_diff']]
+    df_university = pandas.read_json(
+        "data/university_scores_2011_2015.json", orient='records')
+    df_university = df_university.sort_values(
+        ['university', 'year'],
+        ascending=[0, 1]).drop_duplicates()[[
+            'university', 'year', 'score_line', 'avg_score', 'score_diff']]
     return df_university
 
 
 def get_university_scores_with_2015(df_university):
-    university = df_university[df_university['year'] == 2015]['university'].tolist()
+    university = df_university[
+        df_university['year'] == 2015]['university'].tolist()
     df_university = df_university[df_university['university'].isin(university)]
     return df_university
 
@@ -25,43 +31,61 @@ def export_to_csv(df_university, filename):
 
 def merge_ranking(df_university):
     df_ranking = pandas.read_csv('data/scores_ranking_2013_2015.csv')
-    df_university = df_university.merge(df_ranking, how='left', left_on=['year', 'avg_score'], right_on=['year', 'grade']).drop('grade', axis=1).fillna(0)
+    df_university = df_university.merge(
+        df_ranking, how='left',
+        left_on=['year', 'avg_score'],
+        right_on=['year', 'grade']).drop(
+        'grade', axis=1).fillna(0)
     return df_university
 
 
 def merge985_211(df_university):
-    list_985 = pandas.read_csv('data/university_985_list.csv', encoding="utf-8")
-    list_211 = pandas.read_csv('data/university_211_list.csv', encoding='utf-8')
+    list_985 = pandas.read_csv(
+        'data/university_985_list.csv', encoding="utf-8")
+    list_211 = pandas.read_csv(
+        'data/university_211_list.csv', encoding='utf-8')
     list_985 = list_985['985']
     list_211 = list_211['211']
-    df_university['is_985'] = numpy.where(df_university['university'].isin(list_985), '985', 'not_985')
-    df_university['is_211'] = numpy.where(df_university['university'].isin(list_211), '211', 'not_211')
+    df_university['is_985'] = numpy.where(
+        df_university['university'].isin(list_985), '985', 'not_985')
+    df_university['is_211'] = numpy.where(
+        df_university['university'].isin(list_211), '211', 'not_211')
     return df_university
 
 
 def merge_admission(df_university):
-    df_admission = pandas.read_json('data/admission_2012_2013.json', orient='records')
+    df_admission = pandas.read_json(
+        'data/admission_2012_2013.json',
+        orient='records')
     df_university = df_university.merge(
         df_admission,
         how='left',
         left_on=['year', 'university'],
-        right_on=['year', 'university']).drop(['stu_type', 'avg_score_y'], axis=1).fillna(0)
+        right_on=['year', 'university']).drop(
+        ['stu_type', 'avg_score_y'],
+        axis=1).fillna(0)
     return df_university
 
 
 def count_admission(df_university):
     grouped = df_university.groupby(['is_985', 'is_211', 'year'], axis=0)
     # print grouped.count()
-    admission_count = grouped['admission_count'].agg({'admission_count': numpy.sum, 'university': numpy.count_nonzero})
+    admission_count = grouped['admission_count'].agg(
+        {'admission_count': numpy.sum, 'university': numpy.count_nonzero})
     # print admission_count
-    admission_count.to_csv('report/admission_count_2012_2013.csv', encoding="utf-8", index=False)
-    # print grouped['is_985'].agg([numpy.count])
-    # print grouped['admission_count'].agg([numpy.sum])
+    admission_count.to_csv(
+        'report/admission_count_2012_2013.csv', encoding="utf-8", index=False)
+
+
+# def show_985_scores():
+
 
 if __name__ == '__main__':
     df_university = create_df_university_scores()
     df_university = merge_ranking(df_university)
     df_university = merge985_211(df_university)
     df_university = merge_admission(df_university)
-    count_admission(df_university)
-    # export_to_csv(df_university, 'report/all_university_scores_with_ranking.csv')
+    print df_university
+    # count_admission(df_university)
+    # export_to_csv(
+    # df_university, 'report/all_university_scores_with_ranking.csv')
