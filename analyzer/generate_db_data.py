61ebad230882d1853386393f2ea2b93013015738
shujinwu <shujinwu@oregonstate.edu>
2016-08-13 01:55:38 -0700

    update csv

--- analyzer/generate_db_data.py
+++ analyzer/generate_db_data.py
@@ -25,18 +25,34 @@ def createEnrollmentUniversity():
     # ID, StudentPool, year, UniversityID, score_avg, stu_count
     df_enroll_university = pandas.read_csv('report/all_univesity.csv')
     df_university = pandas.read_csv("db_data/university.csv")
-    #  merge id
-    df_enroll_university = df_enroll_university[['university', 'year', 'avg_score', 'admission_count']].drop_duplicates()
-    df_enroll_university['student_pool_id'] = 1
-    df_university = pandas.read_csv("db_data/university.csv")
+    df_enroll_university = df_enroll_university.merge(
+        df_university,
+        how='left',
+        left_on=['university'],
+        right_on=['university'])
 
+    df_enroll_university = df_enroll_university[['university_id', 'year', 'avg_score_x', 'admission_count']].drop_duplicates()
+    df_enroll_university['student_pool_id'] = 1
+    # print df_enroll_university.head()
     export_to_csv(df_enroll_university, "db_data/enroll_university.csv")
 
 
 def createEnrollmentMajor():
     # ID, studentPool_id, universityID, year, major, score_avg
-    pass
+    df_major = pandas.read_csv('report/all_major.csv')
+    df_university = pandas.read_csv("db_data/university.csv")
+    df_major = df_major.merge(
+        df_university,
+        how='left',
+        left_on=['university'],
+        right_on=['university'])
+    print df_major.head()
+    df_major = df_major[['university_id', 'year', 'major', 'avg_major_score']].drop_duplicates().dropna()
+    df_major['student_pool_id'] = 1
+    df_major['university_id'] = df_major['university_id'].astype(numpy.int32)
+    print df_major.head()
+    export_to_csv(df_major, "db_data/enroll_major.csv")
 
 
 if __name__ == '__main__':
-    createUniversities()
+    createEnrollmentMajor()
